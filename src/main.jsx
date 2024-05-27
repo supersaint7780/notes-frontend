401672fb9a7421c5c72879d5d1aba094505d485a
Ayush Maurya <amaurya5556@gmail.com>
2024-05-27 14:24:12 +0530

    Set basic route structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+  Link as RouterLink,
+  Router,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <Router path='/' elment = {<div>Home</div>} />,
+    <Router path='/login' elment = {<div>Login</div>} />, 
+    <Router path='/signup' elment = {<div>Signup</div>} />
+  ])
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
