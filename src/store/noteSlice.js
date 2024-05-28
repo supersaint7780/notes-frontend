ecc98daed2639241fa8e42478f6b3756e67180f8
Ayush Maurya <amaurya5556@gmail.com>
2024-05-28 20:18:23 +0530

    add pin note action to noteStore

--- src/store/noteSlice.js
+++ src/store/noteSlice.js
@@ -59,4 +59,30 @@ export const createNoteSlice = (set) => ({
       console.log("Error:", error);
     }
   },
+  pinNote: async (id) => {
+    try {
+      const response = await fetch(
+        `https://notes-backend-ck0s.onrender.com/api/v1/notes/pin/${id}`,
+        {
+          method: "PATCH",
+          credentials: "include",
+        }
+      );
+      if (response.ok) {
+        const { data } = await response.json();
+        const updatedNote = data;
+        set((state) => ({
+          notes: state.notes.map((note) => {
+            if (note._id === id) {
+              return updatedNote;
+            } else {
+              return note;
+            }
+          }),
+        }));
+      }
+    } catch (error) {
+      console.log("Unable to pin note:", error);
+    }
+  },
 });
